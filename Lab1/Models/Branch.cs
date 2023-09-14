36b044a013e84bf05cc2556f6e929e211a510970
Dang Thai Ha <dth6323@gmail.com>
2023-09-14 19:08:09 +0700

    Upload project

---
+++ Lab1/Models/Branch.cs
@@ -0,0 +1,10 @@
+﻿namespace TH01.Models
+{
+    public enum Branch
+    {
+        IT,
+        BE,
+        CE,
+        EE
+    }
+}
---
+++ Lab1/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace TH01.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
